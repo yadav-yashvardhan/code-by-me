d8061019d47a5278a8d4a4ff98f284b6b0e65967
yadav-yashvardhan <yashvardhanrao8@gmail.com>
2024-09-10 11:11:58 +0530

    Add files via upload

---
+++ hackerrankQnt4.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(1<=n<=9){
+        switch(n){
+
+            
+            case 1:cout<<"ONE";
+            break;
+             case 2:cout<<"TWO";
+            break;
+             case 3:cout<<"THREE";
+            break;
+             case 4:cout<<"FOUR";
+            break;
+             case 5:cout<<"FIVE";
+            break;
+            case 6:cout<<"SIX";
+            break;
+            case 7:cout<<"SEVEN";
+            break;
+            case 8:cout<<"EIGHT";
+            break;
+            case 9:cout<<"NINE";
+            break;
+            
+            
+            
+        }
+        if(n>9){
+            cout<<"GREATER THAN 9";
+        }}}
+            
+
+            
+   
+    
+
+    
